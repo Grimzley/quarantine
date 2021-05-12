f82c9ed5cd97295d2ca42efb6618bdf226c1b752
Michael Grimsley <mgrimsley8460@sdsu.edu>
2021-05-12 01:35:50 -0700

    created Menu scene and script

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Button play;
+    public Button controls;
+    public Button settings;
+    public Button quit;
+
+    public void Start() {
+        play = GameObject.Find("PlayButton").GetComponent<Button>();
+        play.onClick.AddListener(Play);
+
+        controls = GameObject.Find("ControlsButton").GetComponent<Button>();
+        controls.onClick.AddListener(Controls);
+
+        settings = GameObject.Find("SettingsButton").GetComponent<Button>();
+        settings.onClick.AddListener(Settings);
+
+        quit = GameObject.Find("QuitButton").GetComponent<Button>();
+        quit.onClick.AddListener(Quit);
+    }
+    public void Play() {
+        SceneManager.LoadScene("LevelSelection");
+    }
+    public void Controls() {
+        SceneManager.LoadScene("Controls");
+    }
+    public void Settings() {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Quit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
