c22bd9b4984351891d0b6bf570cb744a85ebf151
Michael Grimsley <mgrimsley8460@sdsu.edu>
2021-05-16 21:54:15 -0700

    added reload animation and ammo functionality

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -8,37 +8,73 @@ public class GunController : MonoBehaviour {
 
     // Gun Animations
     public Animator animator;
+    public bool isReloading;
+    public float reloadTime = 2f;
 
     // Gun Sounds
     public AudioSource gun;
     public AudioClip shoot;
+    public AudioClip reload;
 
     // Gun Effects
     public ParticleSystem flash;
 
     // Gun Stats
+    public float damage = 20f;
+    public float force = 50f;
     public float range = 100f;
     public float fireRate = 4f;
     private float timeToFire = 0f;
+    public int maxAmmo = 32;
+    public int currentAmmo;
+    public int magazineSize = 8;
+    public int currentMagazine;
 
     public void Start() {
-        
+        currentAmmo = maxAmmo;
+        currentMagazine = magazineSize;
     }
     public void Update() {
-        if (Input.GetButtonDown("Fire1") && Time.time >= timeToFire) {
+        if (Input.GetButtonDown("Fire1") && Time.time >= timeToFire && !isReloading && currentMagazine > 0) {
             timeToFire = Time.time + 1f / fireRate;
             animator.SetBool("Shoot", true);
             Shoot();
         }else {
             animator.SetBool("Shoot", false);
         }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            ReloadGun();
+        }
     }
     public void Shoot() {
         gun.PlayOneShot(shoot);
         flash.Play();
-        //RaycastHit hit;
-        //if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range)) {
-        //
-        //}
+        RaycastHit hit;
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range)) {
+            
+            if (hit.rigidbody != null) {
+                hit.rigidbody.AddForce(-hit.normal * force);
+            }
+        }
+
+        currentMagazine--;
+        if (currentMagazine == 0) {
+            ReloadGun();
+        }
+    }
+    public void ReloadGun() {
+        if (!isReloading && currentMagazine < magazineSize && currentAmmo > 0) {
+            StartCoroutine(Reload());
+        }
+    }
+    public IEnumerator Reload() {
+        isReloading = true;
+        animator.SetTrigger("Reload");
+        gun.PlayOneShot(reload);
+        yield return new WaitForSeconds(reloadTime);
+        int reloadAmount = Mathf.Clamp(magazineSize - currentMagazine, 0, currentAmmo);
+        currentMagazine += reloadAmount;
+        currentAmmo -= reloadAmount;
+        isReloading = false;
     }
 }
